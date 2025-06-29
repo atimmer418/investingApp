c5f06c180f47dac48429d983a1bd4067354020c8
atimmer <atimmer@terpmail.umd.edu>
2025-06-29 18:05:47 -0400

    added env variables for frontend and backend

--- backend/src/main/java/com/investingapp/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/investingapp/backend/config/SecurityConfig.java
@@ -86,7 +86,8 @@ public class SecurityConfig {
                 "ionic://localhost",
                 "http://localhost",               // Sometimes needed by native wrappers
                 iphoneAngularOrigin,               // Your iPhone's access point
-                "https://37de-2600-4040-2a92-8800-a865-27f2-f55c-bf44.ngrok-free.app"
+                "https://37de-2600-4040-2a92-8800-a865-27f2-f55c-bf44.ngrok-free.app",
+                "http://192.168.1.166:3000"
         ));
         // ngrok
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "PATCH"));
