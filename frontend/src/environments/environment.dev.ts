1ed5a97ed972357d75390aa769a176dd7d1ed4e3
atimmer <atimmer@terpmail.umd.edu>
2025-06-30 23:47:55 -0400

    changed rpId for dev to github codespace and for test to api-test...

--- frontend/src/environments/environment.dev.ts
+++ frontend/src/environments/environment.dev.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false, // It's a test build, not a full production build
     backendApiUrl: 'https://api-dev.fredvested.com/api',
-    rpId: 'app-dev.fredvested.com' // Your test frontend/app origin hostname
+    rpId: 'potential-engine-97999gqqw9q4hpj7w-8100.app.github.dev' // Your test frontend/app origin hostname
   };
--- frontend/src/environments/environment.test.ts
+++ frontend/src/environments/environment.test.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false, // It's a test build, not a full production build
     backendApiUrl: 'https://api-test.fredvested.com/api',
-    rpId: 'potential-engine-97999gqqw9q4hpj7w-8100.app.github.dev' // Your test frontend/app origin hostname
+    rpId: 'api-test.fredvested.com' // Your test frontend/app origin hostname
   };
