012a4116be54d5c91b95b03cdbbe6ea68c45b199
atimmer <atimmer@terpmail.umd.edu>
2025-06-30 21:23:31 -0400

    added dev env and local env properties

---
+++ frontend/src/environments/environment.dev.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false, // It's a test build, not a full production build
+    backendApiUrl: 'https://api-dev.fredvested.com/api',
+    rpId: 'app-dev.fredvested.com' // Your test frontend/app origin hostname
+  };
