178adaee4f16e6857420e975eb1cbbe34d492acb
atimmer <atimmer@terpmail.umd.edu>
2025-11-02 18:35:19 -0500

    Fix EnvironmentInjector error: add standalone:true and remove inject() call

--- frontend/src/app/tabs/tabs.page.ts
+++ frontend/src/app/tabs/tabs.page.ts
@@ -1,4 +1,4 @@
-import { Component, EnvironmentInjector, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { triangle, ellipse, square } from 'ionicons/icons';
@@ -7,10 +7,10 @@ import { triangle, ellipse, square } from 'ionicons/icons';
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss'],
+  standalone: true,
   imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel],
 })
 export class TabsPage {
-  public environmentInjector = inject(EnvironmentInjector);
 
   constructor() {
     addIcons({ triangle, ellipse, square });
