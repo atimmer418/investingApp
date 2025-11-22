5474c046b7192b3badf04a7c7cbb018aafdb1be2
atimmer <atimmer@terpmail.umd.edu>
2025-11-22 15:29:44 -0500

    added sell/withdraw route back

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -74,6 +74,10 @@ export const routes: Routes = [
     loadComponent: () => import('./beneficiaries/add-beneficiary.page').then(m => m.AddBeneficiaryPage)
   },
   {
+    path: 'sell-withdraw',
+    loadComponent: () => import('./sell-withdraw/sell-withdraw.page').then(m => m.SellWithdrawPage)
+  },
+  {
     path: 'tabs',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
   },
